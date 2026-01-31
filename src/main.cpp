5caeeabd89450575e5c5e4c3aa38bf12176b4601
nixnix666 <timurovichroman@gmail.com>
2026-01-31 14:49:11 +0300

    Add sorting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
+int main() {
+    std::string words;
+    std::cin >> words;
+    char letter = ' ';
+    int newpos = 0;
+    std::vector<std::string> wordslist = {};
+    for(int i = 0; i < sizeof(words); i++){
+        letter = words[i];
+        if(letter == ' '){
+            wordslist.push_back(words.substr(newpos, i));
+            newpos = i;
+        }
+    }
 
-int main(){
-    std::cout << "Hui\n";
+    std::sort(wordslist.begin(), wordslist.end());
+
+    for (const std::string& word : wordslist) {
+        std::cout << word << " ";
+    }
+
+    return 0;
 }
